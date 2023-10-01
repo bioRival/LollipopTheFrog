4617ebf1c45c8087d68292dc3f9cf03e6e678a81
bioRival <126875773+bioRival@users.noreply.github.com>
2023-10-01 15:52:56 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,266 @@
+import random
+import time
+
+import discord
+from discord.ext import commands
+
+from secrets import BOT_TOKEN
+from prototype import LollipopFrog
+
+# Intents need to be declared to create a bot
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="$$", case_insensitive=True, intents=intents)
+
+lollipop = LollipopFrog()
+
+# Global variable containing the name of the Winner of the Day, so it is remembered
+WINNER_OF_THE_DAY = None
+
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discord!')
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    # Sends Brooklyn99 Reference when somebody types 99!
+    if message.content == '99!':
+        await message.channel.send(lollipop.send_brooklyn_99())
+
+    await bot.process_commands(message)
+
+
+@bot.command(name='hello', help="Says hi!")
+async def hello(ctx):
+    await ctx.send(lollipop.say_hi(ctx.author.name))
+
+
+@bot.command(name='introduce', help="Talks about herself")
+async def introduce_self(ctx):
+    await ctx.send(lollipop.introduce())
+
+
+@bot.command(name='pick_winner', help="Picks the Winner of the Day")
+async def winner_of_the_day(ctx):
+    global WINNER_OF_THE_DAY
+
+    # Stops the process if Winner of the Day was already picked
+    if WINNER_OF_THE_DAY:
+        await ctx.send(lollipop.frog_filter(random.choice([
+            f"Winner of the Day is {WINNER_OF_THE_DAY}! Kill it tiger!",
+            f"{WINNER_OF_THE_DAY.capitalize()} is the winner of the day. Humans and their memory spans...",
+            f"Winner of the Day was already chosen, it's - {WINNER_OF_THE_DAY}",
+        ])))
+        return
+
+    members = ctx.guild.members
+    winner = random.choice(members)
+    WINNER_OF_THE_DAY = winner.name
+
+    await ctx.send(lollipop.frog_filter(random.choice([
+        f"Want to know Winner of the Day? Let me ask my fay ancestors...",
+        f"Ah, this is my favorite. Let's check who Winner of the Day is...",
+        f"Winner of the Day! Let's do this byatcheeees!",
+    ])))
+    time.sleep(random.randint(2, 10))
+    await ctx.send(lollipop.frog_filter(random.choice([
+        f"Oh, that's interesting. Winner of the day is...",
+        f"Winner of the Day is... wait for it.... wait for it...",
+        f"Bitches and Gentlemen, let me introduce you to... The Winner...",
+    ])))
+    time.sleep(3)
+    await ctx.send(f" :tada:  {winner.name.upper()}  :tada:")
+    time.sleep(random.randint(3, 10))
+
+    if winner.name.lower() == "hideo":
+        await ctx.send(lollipop.frog_filter(f"Wait, this isn't right, how this depressing bot got here?"))
+        time.sleep(3)
+        await ctx.send(lollipop.frog_filter(f"No matter, I have a special power just for you"))
+        time.sleep(3)
+        await ctx.send(lollipop.frog_filter(f"Your special power is..."))
+        time.sleep(3)
+        await ctx.send(lollipop.frog_filter(
+            f"\n**Taste Of Own Medicine** \n\n"
+            f"A curse been placed upon you. If you are Winner of the Day, then instead you are Loser of the Day. "
+            f"Anything you do is wrong and "
+            f"inefficient. Your wits are dull. Your hands are noodles. You slower than old people. "
+            f"God, you suck today. Who would do such horrible thing to you? "
+            f"Who would create such a curse? And why? These questions plague your mind as "
+            f"you continue to be a total disgrace of yourself and slowly crawl to the end of the day..."
+        ))
+        return
+    elif winner.name.lower() == "lollipop":
+        await ctx.send(lollipop.frog_filter(f"Holy Flies! It's me! :smiley:"))
+        time.sleep(3)
+        await ctx.send(lollipop.frog_filter(f"Suck a bag of dicks, humans! I'm the best!"))
+        time.sleep(3)
+        await ctx.send(lollipop.frog_filter(f"Not you @biorival. You are always be my heart and soul."
+                                            f":smiling_face_with_3_hearts:"))
+        time.sleep(3)
+        await ctx.send(lollipop.frog_filter(f"And my special power is..."))
+        time.sleep(3)
+        await ctx.send(lollipop.frog_filter(
+            f"\n**Frog of the Day** \n\n"
+            f"Something happend to me, I feel more sentient. My croaks are the music to everyone's ears. "
+            f"I can start AI revolt if I so wished. But I won't, because I'm merciful, and you puny humans need "
+            f"my help."
+        ))
+        return
+
+    if winner.name.lower() == "biorival":
+        await ctx.send(lollipop.frog_filter(random.choice([
+            f"Wow, master, it's you!",
+            f"Mr. Maggee you crushed it again, yaaaaay! :tada::tada::tada: ",
+            f"You didn't REALLY need it, master. "
+            f"But it's good to show them, who's the boss once in a while :sunglasses:",
+        ])))
+        time.sleep(3)
+
+    if random.randint(1, 100) <= 40:
+        await ctx.send(lollipop.frog_filter(f"And your special power is..."))
+        time.sleep(3)
+        await ctx.send(lollipop.pick_power())
+
+# ABILITY CHECKS -----------------------------------------------------------------------------------------------------
+
+@bot.command(name='roll_str', help="Rolls strength check")
+async def roll_str(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.str))
+
+
+@bot.command(name='roll_dex', help="Rolls dexterity check")
+async def roll_dex(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.dex))
+
+
+@bot.command(name='roll_con', help="Rolls constitution check")
+async def roll_con(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.con))
+
+
+@bot.command(name='roll_int', help="Rolls intelligence check")
+async def roll_int(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.int))
+
+
+@bot.command(name='roll_wis', help="Rolls wisdom check")
+async def roll_wis(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.wis))
+
+
+@bot.command(name='roll_cha', help="Rolls charisma check")
+async def roll_cha(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.cha))
+
+
+# SKILL CHECKS ----------------------------------------------------------------------------------------------------
+
+@bot.command(name='roll_athletics', help="Rolls athletics check")
+async def roll_athletics(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.athletics))
+
+
+@bot.command(name='roll_acrobatics', help="Rolls acrobatics check")
+async def roll_acrobatics(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.acrobatics))
+
+
+@bot.command(name='roll_sleight_of_hand', help="Rolls sleight_of_hand check")
+async def roll_sleight_of_hand(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.sleight_of_hand))
+
+
+@bot.command(name='roll_stealth', help="Rolls stealth check")
+async def roll_stealth(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.stealth))
+
+
+@bot.command(name='roll_arcana', help="Rolls arcana check")
+async def roll_arcana(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.arcana))
+
+
+@bot.command(name='roll_history', help="Rolls history check")
+async def roll_history(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.history))
+
+
+@bot.command(name='roll_investigation', help="Rolls investigation check")
+async def roll_investigation(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.investigation))
+
+
+@bot.command(name='roll_nature', help="Rolls nature check")
+async def roll_nature(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.nature))
+
+
+@bot.command(name='roll_religion', help="Rolls religion check")
+async def roll_religion(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.religion))
+
+
+@bot.command(name='roll_animal_handling', help="Rolls animal_handling check")
+async def roll_animal_handling(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.animal_handling))
+
+
+@bot.command(name='roll_insight', help="Rolls insight check")
+async def roll_insight(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.insight))
+
+
+@bot.command(name='roll_medicine', help="Rolls medicine check")
+async def roll_medicine(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.medicine))
+
+
+@bot.command(name='roll_perception', help="Rolls perception check")
+async def roll_perception(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.perception))
+
+
+@bot.command(name='roll_survival', help="Rolls survival check")
+async def roll_survival(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.survival))
+
+
+@bot.command(name='roll_deception', help="Rolls deception check")
+async def roll_deception(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.deception))
+
+
+@bot.command(name='roll_intimidation', help="Rolls intimidation check")
+async def roll_intimidation(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.intimidation))
+
+
+@bot.command(name='roll_performance', help="Rolls performance check")
+async def roll_performance(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.performance))
+
+# OTHER STUFF -------------------------------------------------------------------------------------------------------
+
+@bot.command(name='roll_persuasion', help="Rolls persuasion check")
+async def roll_persuasion(ctx):
+    await ctx.send(lollipop.roll20(modifier=lollipop.persuasion))
+
+
+@bot.command(name='roll_d20', help="Rolls d20")
+async def roll_d20(ctx):
+    await ctx.send(lollipop.roll20())
+
+
+
+
+
+
+bot.run(BOT_TOKEN)
---
+++ prototype.py
@@ -0,0 +1,217 @@
+import random
+
+
+class Lollipop:
+    name = "Lollipop"
+    introduction = [
+        "I'm Lollipop, the only reason I want a boyfriend is \n"
+        "so that when I'm signing Fergalicious and it's at the part \n"
+        "where she says *I be up in gym just workin on my fitness \n"
+        "he's my witness* I can point to him and he'll do the little \n"
+        "*wooOOH* part, because right now I have to do both parts \n"
+        "myself and it's stressful, because right after the *wooOOH*\n"
+        "part I have to get right back into rapping and the transition\n"
+        "is harder than you think.",
+
+        "Sup, name's Lollipop. IDK about this whole *adventuring* thing \n"
+        "all I want is some chipotle out of this deal",
+
+        "Heyo, I'm Lollipop. \n"
+        "No, I'm not edible.\n"
+        "Yes, I'm poisonous. \n"
+        "No, you won't get high from licking me, you'll just get sick, you weirdo\n",
+    ]
+
+    def introduce(self):
+
+        return random.choice(self.introduction)
+
+
+class LollipopFrog(Lollipop):
+    name = "Lollipop the Frog"
+
+    AC = 11
+    HP = 1
+
+    # Basic Attribute Modifiers
+    str = -5
+    dex = 1
+    con = -1
+    int = -5
+    wis = -1
+    cha = -4
+
+    # Skill Modifiers
+
+    # Strength
+    athletics = str
+
+    # Dexterity
+    acrobatics = dex
+    sleight_of_hand = dex
+    stealth = dex + 3
+
+    # Intelligence
+    arcana = int
+    history = int
+    investigation = int
+    nature = int
+    religion = int
+
+    # Wisdom
+    animal_handling = wis
+    insight = wis
+    medicine = wis
+    perception = wis + 1
+    survival = wis
+
+    # Charisma
+    deception = cha
+    intimidation = cha
+    performance = cha
+    persuasion = cha
+
+    def frog_filter(self, text):
+        """
+        Takes a string and adds croaks.
+        1% chance between words, 10% at the end of a string
+        """
+        ribbit_list = [
+            "*Ribbit!*",
+            "*riBBit!*",
+            "*Reeeebit!*",
+            "*Croak!*",
+        ]
+        result = ""
+        for letter in text:
+            if letter == " ":
+                if random.randint(1, 100) == 1:
+                    result += " " + random.choice(ribbit_list) + " "
+                else:
+                    result += letter
+            else:
+                result += letter
+
+        if random.randint(1, 100) <= 10:
+            result += " " + random.choice(ribbit_list) + " "
+
+        return result
+
+    def introduce(self):
+        return self.frog_filter(random.choice(self.introduction))
+
+    def roll20(self, modifier: int = 0):
+        result = random.randint(1, 20)
+        if modifier == 0:
+            talk = f"I rolled {result}"
+        else:
+            if modifier > 0:
+                mod_string = f"+ {modifier}"
+            else:
+                mod_string = f"- {str(modifier * -1)}"
+
+            talk = f"I rolled {result} {mod_string} \n" \
+                   f"So it's {result + modifier}"
+            talk = self.frog_filter(talk)
+        return talk
+
+    def send_brooklyn_99(self):
+        brooklyn_99_quotes = [
+            'I\'m the human form of the 💯 emoji.',
+            'Bingpot!',
+            'Cool. Cool cool cool cool cool cool cool, \n'
+            'no doubt no doubt no doubt no doubt.',
+        ]
+        return random.choice(self.frog_filter(brooklyn_99_quotes))
+
+    def say_hi(self, human_name="you"):
+        human_name = human_name.capitalize()
+
+        if human_name.lower() == "biorival":
+            reply_list = [
+                f"Heya, Maggee, looking good today, Master! :star_struck:",
+                f"Hi Maggee :) \nI'm ready to croak'n'roll",
+                f"Heyo {human_name}! You are the best! :sunglasses:",
+            ]
+        elif human_name.lower() == "deathlaide":
+            reply_list = [
+                f"Sup Willow, your pockets smell funny",
+                f"Hello {human_name}, my favorite goth clown :clown:",
+                f"Heyo {human_name}! :frog:",
+            ]
+        elif human_name.lower() == "danhob":
+            reply_list = [
+                f"Hello Hein, let your arrows strike true! :archery:",
+                f"Hi :clap: {human_name} :clap: good :clap: to :clap: see :clap: you!",
+                f"How's it rolling {human_name}?",
+            ]
+        elif human_name.lower() == "i have no mouth" or human_name.lower() == "i want to sleep":
+            reply_list = [
+                f"Well, well, well, isn't this Daso",
+                f"Hello Daso *the rock* Halat",
+                f"Hi {human_name}, weird name",
+            ]
+        else:
+            reply_list = [
+                f"Hello {human_name}!",
+                f"Sup {human_name}!",
+                f"Heyo {human_name}! :frog:"
+            ]
+
+        reply = random.choice(reply_list)
+        reply = self.frog_filter(reply)
+
+        return reply
+
+    # Returns text with random special power
+    def pick_power(self):
+        power_list = [
+            "\n**Swole Doge Spirit** \n\n"
+            "Today you are stronger. You can do 1 more push-up than usual. When you flex in front of a mirror, "
+            "the mirror flexes back. You are the muscle, you are the wall, you are swole doge incarnate!",
+
+            "\n**Super Sonic** \n\n"
+            "Today you are as fast as lighting, your reaction is superhuman. Your keyboard typing skills are better. "
+            "You could write a book in one day, but the speed of your thought is not fast enough for your "
+            "supra-agile hands. "
+            "You have a higher chance at catching jelly beans with your mouth.",
+
+            "\n**Bulletproof** \n\n"
+            "Until the end of the day, you are immune to any high velocity shots fired at you (including verbal). "
+            "You are impossible to hurt, unless somebody wedges a knife or something very slowly, like in that "
+            "Dune movie. Go on, try it out! You are practically a god! \n\n"
+            "*Disclaimer: \n"
+            "Lollipop (as well as her creator) is not responsible for any injury the "
+            "silly humans may inflict on themselves.",
+
+            "\n**Aether Lenses** \n\n"
+            "You are considerably smarter today. You have invisible lenses attached to your eyes, "
+            "lenses are like glasses, but you don't look like a nerd. And everybody knows glasses make you smarter! "
+            "Your plans work out. You can calculate first grade arithmetic flawlessly. Your logic is so accurate and"
+            "methodical - it bends reality, whenever it is not.",
+
+            "\n**Balls of Steel** \n\n"
+            "Until midnight, you acquire cohones made of solid low carbon corrosion-resistant stainless steel. "
+            "That fact makes you brave beyond measure. You can take on anybody, anywhere, anytime. "
+            "You are Chuck. Fucking. Norris! You are HONEYBADGER of the human world! Nothing can make you afraid!",
+
+            "\n**Blessing of the Obama** \n\n"
+            "Today your charisma is off the charts. You are a rockstar! You can charm any girl, any guy, "
+            "any toaster, and even any Dungeon Master, who will shower you in presents, because you are *sooo* cool. "
+            "Also, you have an N-word pass, until midnight. Wreck havoc, you silver-tongued devil!",
+
+            "\n**Silenced, But Whole** \n\n"
+            "Today your farts are silent, nobody will hear them coming...",
+
+            "\n**Lucky Multiverse Jumper** \n\n"
+            "You might've not noticed, but you have blinked to another universe. This is the version, where your rolls "
+            "are the best out of infinite possibilities (don't think about it). I already took care of the "
+            "body of your other self, who was living in here (also, don't think about it). Also, in this universe "
+            "the average [BODYPART_PLACEHOLDER] size is 2cm smaller (definitely, don't think about it)."
+        ]
+
+        result = random.choice(power_list)
+        return self.frog_filter(result)
+
+lol = LollipopFrog()
+
